f0a38cf36c991f70f585d132419dcecd1fcf73d3
Canaan129 <55962030+Canaan129@users.noreply.github.com>
2021-04-09 16:11:50 -0700

    Finished the design of AddFragment.

--- app/src/main/java/com/neu/snowhouse/ui/add/AddFragment.java
+++ app/src/main/java/com/neu/snowhouse/ui/add/AddFragment.java
@@ -1,66 +1,108 @@
 package com.neu.snowhouse.ui.add;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.*;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
-
+import android.provider.MediaStore;
+import android.support.v4.*;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.*;
 
 import com.neu.snowhouse.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.io.IOException;
+
+import static android.app.Activity.RESULT_OK;
+
 public class AddFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private EditText enterTitle;
+    private EditText mainBody;
+    private ImageView imageView;
+    private Button post;
+    private Uri selectedImage;
+    private ImageButton cameraImage;
+    private ImageButton folderImage;
+    private String partImage1;
+    private String partImage2;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final int PICK_IMAGE_REQUEST = 9544;
 
-    public AddFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PostFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddFragment newInstance(String param1, String param2) {
-        AddFragment fragment = new AddFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    // Permissions for accessing the storage
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static final String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_add, container, false);
+        enterTitle = view.findViewById(R.id.addTitle);
+        mainBody = view.findViewById(R.id.mainText);
+        imageView = (ImageView) view.findViewById(R.id.image);
+        post = view.findViewById(R.id.postButton);
+        return view;
     }
+
+    public void getImageFromPhotos () {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, false);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, requestCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == PICK_IMAGE_REQUEST) {
+                selectedImage = data.getData();                                                         // Get the image file URI
+                String[] imageProjection = {MediaStore.Images.Media.DATA};
+                @SuppressLint("Recycle") Cursor cursor = getContentResolver().query(selectedImage,
+                        imageProjection, null, null, null);
+                if (cursor != null) {
+                    cursor.moveToFirst();
+                    int indexImage = cursor.getColumnIndex(imageProjection[0]);
+                    partImage1 = cursor.getString(indexImage);
+                    folderImage.setText(partImage1);                                                        // Get the image file absolute path
+                    Bitmap bitmap = null;
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    image.setImageBitmap(bitmap);
+            }
+        }
+    }
+
+
+//    public boolean isStoragePermissionGranted() {
+//        int ACCESS_EXTERNAL_STORAGE = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+//        if ((ACCESS_EXTERNAL_STORAGE != PackageManager.PERMISSION_GRANTED)) {
+//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_PERMISSION);
+//            return false;
+//        }
+//        return true;
+//    }
+
+
 }
+
