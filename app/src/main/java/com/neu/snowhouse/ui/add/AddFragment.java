12f2b02900cc73be8c00f9a18ab9365f7902d632
Canaan129 <55962030+Canaan129@users.noreply.github.com>
2021-04-09 18:00:39 -0700

    Added partial logic into AddFragment.java.

--- app/src/main/java/com/neu/snowhouse/ui/add/AddFragment.java
+++ app/src/main/java/com/neu/snowhouse/ui/add/AddFragment.java
@@ -2,28 +2,19 @@ package com.neu.snowhouse.ui.add;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.app.DownloadManager;
 import android.content.*;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.v4.*;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.*;
-
 import com.neu.snowhouse.R;
-
 import java.io.IOException;
-
 import static android.app.Activity.RESULT_OK;
 
 public class AddFragment extends Fragment {
@@ -50,6 +41,7 @@ public class AddFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
@@ -59,14 +51,16 @@ public class AddFragment extends Fragment {
         mainBody = view.findViewById(R.id.mainText);
         imageView = (ImageView) view.findViewById(R.id.image);
         post = view.findViewById(R.id.postButton);
+        cameraImage = view.findViewById(R.id.addFromCamera);
+        folderImage = view.findViewById(R.id.addFromPhotos);
         return view;
     }
 
     public void getImageFromPhotos () {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType("image/*");
-        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, false);
-        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+        //intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, false);
+        startActivityForResult(Intent.createChooser(intent,"Open Gallery"), PICK_IMAGE_REQUEST);
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -75,34 +69,39 @@ public class AddFragment extends Fragment {
             if (requestCode == PICK_IMAGE_REQUEST) {
                 selectedImage = data.getData();                                                         // Get the image file URI
                 String[] imageProjection = {MediaStore.Images.Media.DATA};
-                @SuppressLint("Recycle") Cursor cursor = getContentResolver().query(selectedImage,
+                @SuppressLint("Recycle") Cursor cursor = getContext().getContentResolver().query(selectedImage,
                         imageProjection, null, null, null);
                 if (cursor != null) {
                     cursor.moveToFirst();
                     int indexImage = cursor.getColumnIndex(imageProjection[0]);
                     partImage1 = cursor.getString(indexImage);
-                    folderImage.setText(partImage1);                                                        // Get the image file absolute path
+                    //folderImage.setText(partImage1);                                                        // Get the image file absolute path
                     Bitmap bitmap = null;
                     try {
-                        bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                        bitmap = MediaStore.Images.Media.getBitmap(this.getContext().getContentResolver(), selectedImage);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                    image.setImageBitmap(bitmap);
+                    imageView.setImageBitmap(bitmap);
+                }
             }
         }
     }
 
-
-//    public boolean isStoragePermissionGranted() {
-//        int ACCESS_EXTERNAL_STORAGE = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-//        if ((ACCESS_EXTERNAL_STORAGE != PackageManager.PERMISSION_GRANTED)) {
-//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_PERMISSION);
-//            return false;
+    //   public static void verifyStoragePermissions(Fragment f) {
+//        // Check if we have write permission
+//        int permission =
+//        //int permission = ActivityCompat.checkSelfPermission(f, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+//
+//        if (permission != PackageManager.PERMISSION_GRANTED) {
+//            // We don't have permission so prompt the user
+//            ActivityCompat.requestPermissions(
+//                    f,
+//                    PERMISSIONS_STORAGE,
+//                    REQUEST_EXTERNAL_STORAGE
+//            );
 //        }
-//        return true;
 //    }
-
 
 }
 
